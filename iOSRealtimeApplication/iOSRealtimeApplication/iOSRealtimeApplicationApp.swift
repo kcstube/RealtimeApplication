8f6507d8bc801ba6d058d51e60f2226ae8835f9c
fummicc1 <fumiya.tennis1@gmail.com>
2021-12-11 16:08:43 +0900

    :tada: プロジェクト作成

---
+++ iOSRealtimeApplication/iOSRealtimeApplication/iOSRealtimeApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSRealtimeApplicationApp.swift
+//  iOSRealtimeApplication
+//
+//  Created by Fumiya Tanaka on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct iOSRealtimeApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
