350a1e49ffc58ca052be9f59e9477c543fc027d9
fummicc1 <fumiya.tennis1@gmail.com>
2021-12-14 16:53:54 +0900

    :sparkles: Server側の実装をした

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
+let sliderValue = 0;
+
+io.on('connection', (socket) => {
+    console.log('新しいクライアントが接続されました');
+
+    socket.on('disconnect', () => {
+        console.log(socket.id, "と切断しました")
+    })
+
+    socket.on("slider", (value) => {
+        sliderValue = value;
+        console.log(value);
+        socket.broadcast.emit("slider", [value]);
+    })
+})
+
+app.get('', (req, res) => {
+})
+
+server.listen(3000);
+
