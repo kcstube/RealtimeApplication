489c10a444ba265f5b5ff5ab1c3ccb5a2f912f56
fummicc1 <fumiya.tennis1@gmail.com>
2022-02-15 23:38:06 +0900

    Many changes

--- server/io.js
+++ server/io.js
@@ -1,3 +1,11 @@
 const index = require('./index');
 const io = index.io;
 
+io.on('connection', (socket) => {
+    console.log('New user:', socket.id);
+
+    socket.on('slider', (slideValue) => {
+        console.log('New slideValue:', slideValue);
+        socket.broadcast.emit('slider', slideValue);
+    });
+});
